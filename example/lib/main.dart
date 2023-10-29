16452491c69dbd281a9ae702eecafaddb7f9ff27
Mohamed khaled Selim <mokhselim@gmail.com>
2023-10-29 21:37:27 +0200

    format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,49 +28,50 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Row(children: [
-        SideBarAnimated(
-          onTap: (s) {},
-          // sideBarColor: Colors.white,
-          // animatedContainerColor: Colors.white,
-          widthSwitch: 700,
-          mainLogoImage: 'assets/logo.png',
-          sidebarItems: [
-            SideBarItem(
-              iconSelected: Icons.home_rounded,
-              iconUnselected: Icons.home_outlined,
-              text: 'Home',
-            ),
-            SideBarItem(
-              iconSelected: Icons.account_balance_wallet,
-              iconUnselected: Icons.account_balance_wallet_outlined,
-              text: 'Insights',
-            ),
-            SideBarItem(
-              iconSelected: CupertinoIcons.chart_bar_square_fill,
-              iconUnselected: CupertinoIcons.chart_bar_square,
-              text: 'Feature',
-            ),
-            SideBarItem(
-              iconSelected: Icons.credit_card_rounded,
-              text: 'Payouts',
-            ),
-            SideBarItem(
-              iconSelected: Icons.settings,
-              iconUnselected: Icons.settings_outlined,
-              text: 'Settings',
-            ),
-          ],
-        ),
-        Expanded(
-          child: Container(
-            alignment: Alignment.topLeft,
-            margin: const EdgeInsets.only(bottom: 20),
+    return Scaffold(
+      body: Row(
+        children: [
+          SideBarAnimated(
+            onTap: (s) {},
+            // sideBarColor: Colors.white,
+            // animatedContainerColor: Colors.white,
+            widthSwitch: 700,
+            mainLogoImage: 'assets/logo.png',
+            sidebarItems: [
+              SideBarItem(
+                iconSelected: Icons.home_rounded,
+                iconUnselected: Icons.home_outlined,
+                text: 'Home',
+              ),
+              SideBarItem(
+                iconSelected: Icons.account_balance_wallet,
+                iconUnselected: Icons.account_balance_wallet_outlined,
+                text: 'Insights',
+              ),
+              SideBarItem(
+                iconSelected: CupertinoIcons.chart_bar_square_fill,
+                iconUnselected: CupertinoIcons.chart_bar_square,
+                text: 'Feature',
+              ),
+              SideBarItem(
+                iconSelected: Icons.credit_card_rounded,
+                text: 'Payouts',
+              ),
+              SideBarItem(
+                iconSelected: Icons.settings,
+                iconUnselected: Icons.settings_outlined,
+                text: 'Settings',
+              ),
+            ],
           ),
-        ),
-      ],),
+          Expanded(
+            child: Container(
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.only(bottom: 20),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
